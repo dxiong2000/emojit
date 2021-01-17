0b83b683a4b99196baac5845c5ac317423829115
Daniel Xiong <dxiong2000@gmail.com>
2021-01-16 17:12:43 -0800

    scrape done pt2

--- scrape.py
+++ scrape.py
@@ -7,11 +7,14 @@ def getEmojiList(searchStr):
     
     soup = BeautifulSoup(page.content, 'html.parser')
     result = soup.find('ol', 'search-results')
-    #print(result)
+    
     elements = result.find_all('span', 'emoji')
     emojis = []
     for e in elements:
-        emojis.append(e.contents[0])
+        emoji = e.contents[0].strip(' ')
+        if len(emoji) > 1:
+            emoji = emoji[0]
+        emojis.append(f'U+{ord(emoji):X}')
     
     emojiNames = []
     elements = result.find_all('a')
@@ -19,8 +22,7 @@ def getEmojiList(searchStr):
         emojiNames.append(e.contents[1].strip(' '))
     
     emojiDict = dict(zip(emojiNames, emojis))
+    
     print(emojiDict)
     return emojiDict
-    
-getEmojiList('tree')
     
