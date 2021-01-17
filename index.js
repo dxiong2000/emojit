c46a3bf3b0d76cfcb5c59c142daab68c277c1727
Daniel Xiong <dxiong2000@gmail.com>
2021-01-16 16:21:21 -0800

    x

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const endpoints = require('./endpoints.js')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+/**
+ * localhost:3000/getEmoji?search=[SearchText]
+ */
+app.get('/getEmoji', endpoints.getEmoji);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- server.py
+++ server.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# /getEmoji?search=SearchStr
+@app.route('/getEmoji')
+def getEmoji():
+    searchStr = request.args.get('search')
+    
